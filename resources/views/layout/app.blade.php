958351f1628957c202cc42fa953119bb6d10c4ef
winigajordan <winigajordan@gmail.com>
2022-09-05 20:12:19 +0000

    debut compilation des assets avec vite

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,9 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/style.css" rel="stylesheet">
+    @vite(['resources/css/app.css'])
 </head>
 <body>
 @include('partials.navbar')
+<img src="{{URL::asset('img/2.png')}}" alt="jordan">
 @yield('body')
 </body>
 </html>
